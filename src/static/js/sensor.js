935f692f870e6e6027890f64b4c1a230fc3ec735
Shaun Crampton <Shaun.Crampton@metaswitch.com>
2012-05-04 20:14:33 -0700

    Implement graph.

--- src/static/js/sensor.js
+++ src/static/js/sensor.js
@@ -16,5 +16,29 @@ $(window).load(function(){
   // ---------------------------------------------------------------------------
   // Startup.
   // ---------------------------------------------------------------------------
+  function drawChart() {
+
+    jQuery.getJSON("/readings", null, function(data) {
+      log("Downloaded " + data["readings"].length + " readings");
+      window.sensors["data"] = data;
+      var chartData = new google.visualization.DataTable();
+      chartData.addColumn('number', 'Date');
+      chartData.addColumn('number', 'Temperature/C');
+      chartData.addRows(data["readings"]);
+      
+      // Set chart options
+      var options = {'title':'Temperature',
+                     'width': 900,
+                     'height':600};
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      chart.draw(chartData, options);
+    })
+    .error(function() {
+      log("Failed to load readings");
+    });
+  }
   
+  drawChart();
 });
